12f5f6c9a56a92be27d1f6f4326a3749b64693af
Rouzax <github@mgdn.nl>
2023-08-18 11:35:39 +0200

    Added Test Encode to see bitrate estimation

--- HandBrakeCLI-PowerShell.ps1
+++ HandBrakeCLI-PowerShell.ps1
@@ -13,7 +13,17 @@ param (
     [Parameter()]
     [switch]$ConvertOnly,
 
-    [string]$HandBrakeCLI
+    [Parameter()]
+    [string]$HandBrakeCLI,
+
+    [Parameter()]
+    [string]$FFprobePath,
+
+    [Parameter()]
+    [switch]$TestEncode,
+
+    [Parameter()]
+    [int]$TestEncodeSeconds = 120
 )
 
 
@@ -75,15 +85,42 @@ function Select-MenuOption {
     }
 }
 
+# Function to convert bitrate to human-readable format with two decimal places
+function Convert-BitRate($bitRate) {
+    <#
+    .SYNOPSIS
+    This function converts a given bit rate value into a human-readable format, including bps, kbps, and Mbps.
+    
+    .DESCRIPTION
+    The Convert-BitRate function takes a bit rate value as input and converts it into a more readable format. It calculates and rounds the bit rate to kilobits per second (kbps) and megabits per second (Mbps) as appropriate, and then returns the formatted result with the corresponding unit.
+
+    .PARAMETER bitRate
+    Specifies the bit rate value that needs to be converted. It should be provided in bits per second (bps).
+
+    .EXAMPLE
+    Example 1:
+    Convert-BitRate -bitRate 2500000
+    This example converts a bit rate of 2500000 bps into 2.50 Mbps.
+    #>
+    if ($null -eq $bitRate) {
+        return ""
+    }
+
+    $kbps = [math]::Round($bitRate / 1000, 2)
+    $mbps = [math]::Round($kbps / 1000, 2)
+
+    if ($mbps -ge 1) {
+        return ("{0:N2}" -f $mbps) + " Mbps"
+    } elseif ($kbps -ge 1) {
+        return ("{0:N2}" -f $kbps) + " kbps"
+    } else {
+        return "${bitRate} bps"
+    }
+}
+
 # Set default value for $HandBrakeCLI if not provided
 if (-not $HandBrakeCLI) {
     $HandBrakeCLI = "C:\Program Files\HandBrake\HandBrakeCLI.exe"
-}
-
-# Check if the OutputFolder exists and create it if not
-if (-not (Test-Path -Path $OutputFolder -PathType 'Container')) {
-    New-Item -ItemType Directory -Path $OutputFolder | Out-Null
-    Write-Host "Created OutputFolder: $OutputFolder"
 }
 
 # Validate if HandBrakeCLI.exe exists
@@ -92,8 +129,27 @@ if (-not (Test-Path -Path $HandBrakeCLI -PathType 'Leaf')) {
     Exit
 }
 
-# Get input if no parameters defined, list all json presets
+if ($TestEncode) {
+    if (-not $FFprobePath) {
+        $FFprobePath = "C:\Program Files\FFmpeg\ffprobe.exe"
+    }
+    # Validate if FFprobe.exe exists
+    if (-not (Test-Path -Path $FFprobePath -PathType 'Leaf')) {
+        Write-Host "FFprobe.exe not found at the specified path: $FFprobePath"
+        Exit
+    }
+} else {
+    $startFullEncode = $true
+}
 
+# Check if the OutputFolder exists and create it if not
+if (-not (Test-Path -Path $OutputFolder -PathType 'Container')) {
+    New-Item -ItemType Directory -Path $OutputFolder | Out-Null
+    Write-Host "Created OutputFolder: $OutputFolder"
+}
+
+
+# Get input if no parameters defined, list all json presets
 if ($PSBoundParameters.ContainsKey('PresetFile')) {
     Write-Host "Preset File given as Parameter"
 } else {
@@ -111,19 +167,78 @@ $PresetName = $JsonContent.PresetList[0].PresetName
 # Get all files in the source folder and subfolders
 $Files = Get-ChildItem -Path $SourceFolder -File -Recurse
 
-# Loop through each file
-foreach ($File in $Files) {
-    # Get the folder path and file name
-    $SourceFilePath = $File.FullName
-    $SourceFileRelativePath = $SourceFilePath.Substring($SourceFolder.Length + 1)
-    $OutputFilePath = Join-Path -Path $OutputFolder -ChildPath $SourceFileRelativePath
 
-    # Create the output folder if it doesn't exist
-    $OutputFileFolder = Split-Path -Path $OutputFilePath
-    if (-not (Test-Path -Path $OutputFileFolder -PathType 'Container')) {
-        New-Item -ItemType Directory -Path $OutputFileFolder | Out-Null
+while (-not $startFullEncode) {
+    # Array to store bitrate information
+    $bitRateInfo = @()
+    # Loop through each file
+    foreach ($File in $Files) {
+        # Get the folder path and file name
+        $SourceFilePath = $File.FullName
+        $SourceFileRelativePath = $SourceFilePath.Substring($SourceFolder.Length + 1)
+        $OutputFilePath = Join-Path -Path $OutputFolder -ChildPath $SourceFileRelativePath
+
+        # Create the output folder if it doesn't exist
+        $OutputFileFolder = Split-Path -Path $OutputFilePath
+        if (-not (Test-Path -Path $OutputFileFolder -PathType 'Container')) {
+            New-Item -ItemType Directory -Path $OutputFileFolder | Out-Null
+        }
+
+        if ($TestEncode) {
+            $startFullEncode = $false
+
+            # Use FFprobe to get Source video information
+            $ffprobeOutput = & $FFprobePath -v error -print_format json -show_format -show_streams "$SourceFilePath" | ConvertFrom-Json
+            $sourceVideoDuration = [int]$ffprobeOutput.format.duration
+            if ($sourceVideoDuration -le $TestEncodeSeconds) {
+                $startAt = 0
+                $endAt = $sourceVideoDuration
+            } else {
+                [int]$sourceVideoMidPoint = ($sourceVideoDuration / 2)
+                $startAt = $sourceVideoMidPoint - ($TestEncodeSeconds / 2)
+                $endAt = $TestEncodeSeconds
+            }
+            $sourceBitRate = $ffprobeOutput.format.bit_rate
+            $sourceBitRateFormatted = Convert-BitRate $sourceBitRate
+
+            # Run test encode with options
+            & $HandBrakeCLI --preset-import-file "$PresetFile" --preset "$PresetName" --input "$SourceFilePath" --output "$OutputFilePath" --start-at "seconds:$startAt" --stop-at "seconds:$endAt"
+        
+            # Use FFprobe to get bit rate of test encode
+            $ffprobeTestOutput = & $FFprobePath -v error -print_format json -show_format "$OutputFilePath" | ConvertFrom-Json
+            $testBitRate = $ffprobeTestOutput.format.bit_rate
+            $testBitRateFormatted = Convert-BitRate $testBitRate
+
+            $bitRateInfo += [PSCustomObject]@{
+                FileName          = $File.Name
+                "Source Bit Rate" = $sourceBitRateFormatted
+                "Test Bit Rate"   = $testBitRateFormatted
+            }
+        }
     }
 
+    Clear-Host
+    
+    $bitRateInfo | Format-Table -AutoSize FileName, "Source Bit Rate", "Test Bit Rate"
+    $response = Read-Host "Is the Bit Rate okay? (Y/N)"
+    if ($response -eq 'Y' -or $response -eq 'y') {
+        $startFullEncode = $true
+    } elseif ($response -eq 'N' -or $response -eq 'n') {
+        # Prompt to select a different preset
+        $SelectedPreset = Select-MenuOption -MenuOptions $PresetFiles -MenuQuestion "Handbrake Preset"
+        $PresetFile = $SelectedPreset.FullName
+   
+        # Read the JSON preset file
+        $JsonContent = Get-Content -Path $PresetFile | ConvertFrom-Json
+    
+        # Get the preset name
+        $PresetName = $JsonContent.PresetList[0].PresetName
+
+        # Don't start full encode just yet
+        $startFullEncode = $false
+    }
+}
+if ($startFullEncode) {
     if (($File.Extension -eq '.mkv') -or ($File.Extension -eq '.mp4')) {
         # Convert MKV files with HandBrakeCLI
         & $HandBrakeCLI --preset-import-file "$PresetFile" --preset "$PresetName" --input "$SourceFilePath" --output "$OutputFilePath"
